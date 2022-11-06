1884ef15887f1b14e35599a13e46f989975ae5c0
Greg French <gfrench@uoguelph.ca>
2022-11-06 04:41:11 +0000

    Updated worker

--- worker.sh
+++ worker.sh
@@ -13,19 +13,20 @@ if [ ! -p /tmp/worker-$ID-$USER-inputfifo ] ; then
     mkfifo /tmp/worker-$ID-$USER-inputfifo
 fi
 
-echo "ready" > /tmp/worker-$ID-$USER-inputfifo;
+echo "ready" > /tmp/worker-$ID-$USER-inputfifo &
 
 count=0
 while [ $terminate != 0 ]
 do
     if read line; then
-        if [ $count -eq 0 ]; then
-            $line > /tmp/worker-$USER.$ID.log
-        else
-            $line >> /tmp/worker-$USER.$ID.log
+        if [ "$line" != "ready" ]; then
+            if [ $count -eq 0 ]; then
+                $line > /tmp/worker-$USER.$ID.log
+            else
+                $line >> /tmp/worker-$USER.$ID.log
+            fi
+            count=$(($count+1))
+            echo "ready" > /tmp/worker-$ID-$USER-inputfifo &
         fi
-
-        count=$(($count+1))
-        echo "ready" > /tmp/worker-$ID-$USER-inputfifo;
     fi
 done </tmp/worker-$ID-$USER-inputfifo

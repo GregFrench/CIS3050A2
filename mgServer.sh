d482402c0778694342b1dd58eb6d8f67a62c233d
Greg French <gfrench@uoguelph.ca>
2022-11-06 23:54:18 +0000

    Updated mgServer

--- mgServer.sh
+++ mgServer.sh
@@ -4,9 +4,6 @@ USER=gfrench
 terminate=1
 pipe=/tmp/server-$USER-inputfifo
 
-## Setup signal trap
-trap 'trap " " SIGINT SIGKILL; kill 0; wait; sigterm_handler' SIGINT SIGKILL
-
 if [ ! -p $pipe ] ; then 
     mkfifo $pipe
 fi
@@ -14,19 +11,24 @@ fi
 numWorkers=`cat /proc/cpuinfo | grep processor | wc -l`
 workers=()
 worker=0
+numProcessed=0
 
 echo "Starting up ${numWorkers} processing units"
 
-sigterm_handler() { 
+shutdownHandler() {
+    echo "handler called"
     i=0
     while [ $i -ne $numWorkers ]
     do
         i=$(($i+1))
         echo "shutdown" > /tmp/worker-$i-$USER-inputfifo &
+        echo "test"
     done
 
     exit 1
 }
+
+trap 'shutdownHandler' SIGINT
 
 i=0
 while [ $i -ne $numWorkers ]
@@ -41,22 +43,21 @@ echo "Ready for processing : place tasks into /tmp/server-${USER}-fifo"
 while [ $terminate != 0 ]
 do
     if read line; then
-        echo $line
         str="$line"
         set -- $str
         firstWord=$1
 
-        if [[ "$firstWord" == "shutdown" ]]; then
-            sigterm_handler
+        if [[ "$line" == "shutdown" ]]; then
             terminate=0
+        elif [[ "$firstWord" == "status" ]]; then
+            echo "Number of workers: ${numWorkers}"
+            echo "Number of tasks processed: ${numProcessed}"
         elif [[ "$firstWord" == "ready" ]]; then
-            echo "id:"
             ID=$(echo $line | cut -d " " -f2-)
-            echo $ID
             workers[$(($ID-1))]=1
+            numProcessed=$(($numProcessed+1))
         else
             if [[ workers[$(($worker-1))] -eq 1 ]]; then
-                echo "go"
                 echo $line | cut -d " " -f2- > /tmp/worker-$(($worker+1))-$USER-inputfifo;
                 workers[$(($worker-1))]=0
                 worker=$(($worker+1))
@@ -67,3 +68,5 @@ do
         fi
     fi
 done <$pipe
+
+shutdownHandler

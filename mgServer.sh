d0fc9643d809eb1864cda5514f7953d477d3c0f3
Greg French <gfrench@uoguelph.ca>
2022-11-06 23:00:51 +0000

    Updated mgServer

--- mgServer.sh
+++ mgServer.sh
@@ -4,18 +4,6 @@ USER=gfrench
 terminate=1
 pipe=/tmp/server-$USER-inputfifo
 
-sigterm_handler() { 
-    i=0
-    while [ $i -ne $numWorkers ]
-    do
-        i=$(($i+1))
-        echo "remove";
-        rm "/tmp/worker-$i-$USER-inputfifo"
-    done
-
-    exit 1
-}
-
 ## Setup signal trap
 trap 'trap " " SIGINT SIGKILL; kill 0; wait; sigterm_handler' SIGINT SIGKILL
 
@@ -24,17 +12,28 @@ if [ ! -p $pipe ] ; then
 fi
 
 numWorkers=`cat /proc/cpuinfo | grep processor | wc -l`
-arr=()
+workers=()
 worker=0
 
 echo "Starting up ${numWorkers} processing units"
+
+sigterm_handler() { 
+    i=0
+    while [ $i -ne $numWorkers ]
+    do
+        i=$(($i+1))
+        echo "shutdown" > /tmp/worker-$i-$USER-inputfifo &
+    done
+
+    exit 1
+}
 
 i=0
 while [ $i -ne $numWorkers ]
 do
     i=$(($i+1))
     ./worker.sh $i &
-    arr+=($!)
+    workers+=(1)
 done
 
 echo "Ready for processing : place tasks into /tmp/server-${USER}-fifo"
@@ -43,13 +42,28 @@ while [ $terminate != 0 ]
 do
     if read line; then
         echo $line
-        if [[ "$line" == "shutdown" ]]; then
+        str="$line"
+        set -- $str
+        firstWord=$1
+
+        if [[ "$firstWord" == "shutdown" ]]; then
             sigterm_handler
             terminate=0
+        elif [[ "$firstWord" == "ready" ]]; then
+            echo "id:"
+            ID=$(echo $line | cut -d " " -f2-)
+            echo $ID
+            workers[$(($ID-1))]=1
         else
-            echo $line | cut -d " " -f2- > /tmp/worker-$(($worker+1))-$USER-inputfifo;
-            worker=$(($worker+1))
-            worker=$(($worker%$numWorkers))
+            if [[ workers[$(($worker-1))] -eq 1 ]]; then
+                echo "go"
+                echo $line | cut -d " " -f2- > /tmp/worker-$(($worker+1))-$USER-inputfifo;
+                workers[$(($worker-1))]=0
+                worker=$(($worker+1))
+                worker=$(($worker%$numWorkers))
+            else
+                echo $line > /tmp/server-$USER-inputfifo;
+            fi
         fi
     fi
 done <$pipe

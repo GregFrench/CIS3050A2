fa1405f47b278df0e6c53171eddef6a15508d232
Greg French <gregfrench26@gmail.com>
2022-11-13 21:04:50 +0000

    Updated runtests

--- runtests.sh
+++ runtests.sh
@@ -1,2 +1,20 @@
+#! /bin/bash
+
+# First test case
 ./submitJob.sh ls -l &
+
+# Second test case
 ./submitJob.sh ./timedCountdown.sh 10 &
+
+echo "Waiting for jobs to finish..."
+
+# sleep for 12 seconds
+sleep 12
+
+# Third test case
+./submitJob.sh -s &
+
+sleep 1
+
+# Fourth test case
+./submitJob.sh -x &

e1e5b16b6e51c87b9c4783bdc1e581456670fba9
Greg French <gregfrench26@gmail.com>
2022-11-13 21:11:18 +0000

    Added tests to runtests

--- runtests.sh
+++ runtests.sh
@@ -6,15 +6,24 @@
 # Second test case
 ./submitJob.sh ./timedCountdown.sh 10 &
 
+# Third test case
+./submitJob.sh ls | sort --ignore-case &
+
+# Fourth test case
+./submitJob.sh ./timedCountdown.sh 5 &
+
+# Fifth test case
+./submitJob.sh ls | sort -r --ignore-case &
+
 echo "Waiting for jobs to finish..."
 
 # sleep for 12 seconds
 sleep 12
 
-# Third test case
+# Sixth test case
 ./submitJob.sh -s &
 
 sleep 1
 
-# Fourth test case
+# Seventh test case
 ./submitJob.sh -x &

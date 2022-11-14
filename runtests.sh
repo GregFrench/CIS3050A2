f1166ad0ffe6e7818a0a2aa6b9638cfda0ecae4c
Greg French <gregfrench26@gmail.com>
2022-11-14 01:17:54 +0000

    Added echo statements to runtests

--- runtests.sh
+++ runtests.sh
@@ -1,34 +1,76 @@
 #! /bin/bash
 
+echo "Submitting jobs..."
+
 # First test case
 ./submitJob.sh ls -l &
+
+echo "Submitting job 1"
+
+sleep 1
 
 # Second test case
 ./submitJob.sh ./timedCountdown.sh 10 &
 
+echo "Submitting job 2"
+
+sleep 1
+
 # Third test case
 ./submitJob.sh ls | sort --ignore-case &
+
+echo "Submitting job 3"
+
+sleep 1
 
 # Fourth test case
 ./submitJob.sh ./timedCountdown.sh 5 &
 
+echo "Submitting job 4"
+
+sleep 1
+
 # Fifth test case
 ./submitJob.sh ls | sort -r --ignore-case &
+
+echo "Submitting job 5"
+
+sleep 1
 
 # Sixth test case
 ./submitJob.sh ./timedCountdown.sh 2 &
 
+echo "Submitting job 6"
+
+sleep 1
+
 # Seventh test case
 ./submitJob.sh echo "Hello, World" &
+
+echo "Submitting job 7"
+
+sleep 1
 
 # Eighth test case
 ./submitJob.sh pwd &
 
+echo "Submitting job 8"
+
+sleep 1
+
 # Ninth test case
 ./submitJob.sh ls &
 
+echo "Submitting job 9"
+
+sleep 1
+
 # Tenth test case
 ./submitJob.sh ls -F &
+
+echo "Submitting job 10"
+
+sleep 1
 
 echo "Waiting for jobs to finish..."
 
@@ -38,7 +80,15 @@ sleep 12
 # Eleventh test case
 ./submitJob.sh -s &
 
+echo "Getting status"
+
 sleep 1
 
 # Twelfth test case
 ./submitJob.sh -x &
+
+echo "Shutting down"
+
+sleep 1
+
+echo "Finished running tests"
